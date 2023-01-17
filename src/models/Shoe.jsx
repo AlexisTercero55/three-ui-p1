972b7845761b15bef3b70821f5d2627a342ad866
Alexis Tercero <alexistercero55@gmail.com>
2023-01-16 20:28:27 -0600

    some imports changes

--- src/models/Shoe.jsx
+++ src/models/Shoe.jsx
@@ -8,7 +8,7 @@ import { useGLTF } from '@react-three/drei'
 import { useFrame } from '@react-three/fiber';
 
 export function Model(props) {
-  const { nodes, materials } = useGLTF('/assets/gltf/shoe/shoe.gltf')
+  const { nodes, materials } = useGLTF('./assets/gltf/shoe/shoe.gltf')
   const shoe = useRef();
   useFrame((state, delta, xrFrame) => {
     shoe.current.rotateY(delta/10);
